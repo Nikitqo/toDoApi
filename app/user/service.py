2781d6c8b2aee8e55f6f25648eff2afc7eeac956
Nikitqo <n.kurmanov@mail.ru>
2023-07-26 17:01:58 +0300

    refactored code

--- app/user/service.py
+++ app/user/service.py
@@ -11,22 +11,18 @@ def get_password_hash(password):
 
 def add_new_user(data):
     if find_user_by_email(data.email):
-        user_for_insert = {
-            'username': data.username,
-            'password_hash': get_password_hash(data.password.get_secret_value()),
-            'email': data.email
-        }
-        users.insert_one(user_for_insert)
-        return {"data": f'Пользователь: {data.username} успешно зарегистрирован!'}
-    else:
         raise HTTPException(
             status_code=400,
             detail=[{"error": f'Пользователь с email: {data.email} уже существует'}]
         )
+    user_for_insert = {
+        'username': data.username,
+        'password_hash': get_password_hash(data.password.get_secret_value()),
+        'email': data.email
+    }
+    users.insert_one(user_for_insert)
+    return {"data": f'Пользователь: {data.username} успешно зарегистрирован!'}
 
 
 def find_user_by_email(email):
-    if users.find_one({"email": email}) is None:
-        return True
-    else:
-        return False
+    return users.find_one({"email": email})
