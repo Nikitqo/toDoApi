d6b4ba39ffeb45bb27fc65dfc1eac64c433b922b
Nikitqo <n.kurmanov@mail.ru>
2023-07-27 23:02:04 +0300

    added token model

--- app/user/service.py
+++ app/user/service.py
@@ -4,8 +4,10 @@ from fastapi import HTTPException
 from passlib.context import CryptContext
 from app.database import users
 from jose import JWTError, jwt
-
+from app.user import Token
 # token var
+
+
 SECRET_KEY = "09d25e054faa6ca2552c818166b7a9563b93f7091f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -69,4 +71,4 @@ def login_user_by_email(data):
     access_token = create_access_token(
         data={"sub": data.email}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(access_token=access_token, token_type="bearer")
