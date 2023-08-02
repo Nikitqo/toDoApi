c913a3285c4d0eb6a50f54c2171bd4b7367c7005
Nikitqo <n.kurmanov@mail.ru>
2023-08-03 00:51:25 +0300

    added kwargs

--- app/tasks/service.py
+++ app/tasks/service.py
@@ -6,9 +6,7 @@ from app.tasks import State
 # main logic
 def add_new_task(data, user_id):
     task_for_insert = {
-        'name': data.name,
-        'description': data.description,
-        'deadline': data.deadline,
+        **data,
         'state': State.Created,
         'created_at': datetime.utcnow(),
         'user_id': user_id
