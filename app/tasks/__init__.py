0cdab1a92772dd9edd4f400430565c4dbcdf0ae5
Nikitqo <n.kurmanov@mail.ru>
2023-08-03 10:22:41 +0300

    added delete func

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,2 +1,2 @@
 from .models import State, CreateTask, UpdateTask, Task
-from .service import add_new_task
+from .service import add_new_task, delete_task_by_id
--- app/tasks/service.py
+++ app/tasks/service.py
@@ -1,15 +1,50 @@
 from datetime import datetime
+
+import bson
+from fastapi import HTTPException
+from bson import ObjectId
 from app.database import tasks
 from app.tasks import State
 
 
+def find_task_by_id(_id):
+    return tasks.find_one({"_id": ObjectId(_id)})
+
+
+def verify_user(user_id_from_db, user_id):
+    if user_id_from_db != user_id:
+        raise HTTPException(
+            status_code=403,
+            detail=[{"error": 'Доступ запрещен'}]
+        )
+    return True
+
+
 # main logic
-def add_new_task(data, user_id):
+def add_new_task(task, user_id):
     task_for_insert = {
-        **data,
+        **task.__dict__,
         'state': State.Created,
         'created_at': datetime.utcnow(),
         'user_id': user_id
     }
     tasks.insert_one(task_for_insert)
-    return {"data": f'Задача: {data.name} создана'}
+    return {"message": f'Задача: {task.name} создана'}
+
+
+def delete_task_by_id(task_id, user_id):
+    try:
+        task = find_task_by_id(task_id)
+        if not task:
+            raise HTTPException(
+                status_code=404,
+                detail=[{"error": 'Задача не найдена'}]
+            )
+        if verify_user(task['user_id'], user_id):
+            tasks.delete_one({'_id': ObjectId(task_id)})
+            return {"message": f'Задача: {task["name"]} удалена'}
+    except bson.errors.InvalidId:
+        return HTTPException(
+                status_code=400,
+                detail=[{f'{task_id} is not a valid ObjectId, it must be a 12-byte input or a 24-character hex string'}]
+            )
